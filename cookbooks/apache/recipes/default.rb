d74d0ed9b61ba1b6c30fe4589ef56df1f63de9d8
Roberto <rnovo1983g@gmail.com>
2017-03-04 01:54:24 -0500

    Added motd

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -3,13 +3,19 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+include_recipe 'apache::motd'
 
-package 'apache2' do
+if node['platform_family'] == "debian"
+        package = "apache2"
+elseif node['platform_family'] == "rhel"
+        package = "httpd"
+end
+
+package 'httpserver' do
+     package_name package
      action :install
 end
 
 service 'apache2' do
     action [:start, :enable]
 end
-
-
